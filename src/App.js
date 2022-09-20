d316bfe14b507538e808b552ee38825f7fe94f18
Chukwujiobi <chukwujiobicanon@gmail.com>
2022-09-20 12:55:39 +0100

    Revert "Frontend version 1.0 ready"
    This reverts commit f4f620317c11bf5d7bee7262bda436dfd3837efa.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import { Route, Routes, HashRouter } from "react-router-dom";
-import Account from "./Pages/Account/Account";
-import Home from "./Pages/Home/Home";
-import MyPosts from "./Pages/Home/MyPosts";
-import Publish from "./Pages/Publish/Publish";
-import Signin from "./Pages/Signin/Signin";
-import SingleArticle from "./Pages/Single article/SingleArticle";
-import SingleComment from "./Pages/Single article/SingleComment";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/publish" element={<Publish />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/article/:id" element={<SingleArticle />} />
-        <Route path="/myarticles" element={<MyPosts />} />
-        <Route path="/comment/:id" element={<SingleComment />} />
-      </Routes>
-    </HashRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
